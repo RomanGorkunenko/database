60395d80154fb68319a3e5ea7300d112f9eb06cf
RomanGorkunenko <35932569+RomanGorkunenko@users.noreply.github.com>
2018-03-16 00:12:51 +0200

    Update ModelBaseFunction.php

--- app/Models/ModelBaseFunction.php
+++ app/Models/ModelBaseFunction.php
@@ -3,17 +3,8 @@ namespace App\Models;
 use Illuminate\Support\Facades\DB;
 class ModelBaseFunction
 {
-    //private $table_name = 'users';
     public function getAllUsers() {
-        //return DB::select('select * from users');
-        //return DB::table('users')->get();
         return DB::table($this->table_name)->get();
-        //return DB::table($this->table_name)
-              //  ->where('id', $id)
-                //->where('id','!=', 5)
-                //->select('email')
-                //->where('id', 'like', 5)
-               // ->get();
     }
     
     public function getCountUsers() {
